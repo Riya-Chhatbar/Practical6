ddc111b097c1ee8c764acbef7075236364ff46bf
Riya-Chhatbar <riya.chhatbar131035@marwadiuniversity.ac.in>
2026-09-23 10:16:40 +0530

    Add files via upload

---
+++ ProductController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using program6.Models;
+
+namespace program6.Controllers
+{
+    public class ProductController : Controller
+    {
+        static List<product> products = new List<product>
+        {
+            new product
+            {
+                Id = 1,
+                Name = "Laptop",
+                Category = "Electronics",
+                Price = 50000,
+                Description = "HP Laptop",
+                ImageUrl = "laptop.pgn"
+            },
+
+            new product
+            {
+                Id = 2,
+                Name = "Mobile",
+                Category = "Electronics",
+                Price = 25000,
+                Description = "Android Mobile",
+                ImageUrl = "mobile.webp"
+            }
+        };
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(products);
+        }
+
+    
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            product p = products.FirstOrDefault(x => x.Id == id);
+
+            if (p == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(p);
+        }
+    }
+}
---
+++ product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace program6.Models
+{
+    public class product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
+
+   
